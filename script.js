b40b6d1f00a793ff44cac78d7f00e1abe9c5dc2a
Damien <damien.deboudt@sfr.fr>
2022-12-21 13:12:37 +0100

    ok, bonus 1 ok, reste bonus 2

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
 let progress = $('.progress__filled').first();
+let buttonPlay = $('.player__button').first()
+let bool = true
+console.log(bool)
+
+    //condition avec booléen
+    buttonPlay.on('click', ()=>{
+        if (bool === true) {
+            player.get(0).play()
+            bool = false
+            buttonPlay.html("| |")
+        }
+        else{
+            player.get(0).pause()
+            bool = true
+            buttonPlay.html("►")
+        }
+    })
+
+        //changement volume
+$('#volume').change(function() {
+    player.prop('volume', this.value);
+});
+
+    //barre progression
+player.on('timeupdate', function() {
+    let progress = (this.currentTime / this.duration) * 100;
+    $('.progress__filled').css('width', progress + '%');
+});
